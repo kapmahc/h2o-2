a506e43c062d377d69e931dee044e394402a2257
Jeremy Zheng <jitang.zheng@gmail.com>
2017-07-20 10:06:00 -0700

    use syslog

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 extern crate h2o;
-extern crate env_logger;
+// extern crate env_logger;
+extern crate syslog;
+extern crate log;
 
 fn main() {
-    env_logger::init().unwrap();
+    // env_logger::init().unwrap();
+    syslog::init(
+        syslog::Facility::LOG_USER,
+        log::LogLevelFilter::max(),
+        None).unwrap();
     h2o::app::run()
 }
