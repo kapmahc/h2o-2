95ea7948f1a131177ba9e3abf49e3ff999839800
Jitang Zheng <jitang.zheng@gmail.com>
2017-07-21 17:12:30 -0700

    rename application name

--- src/main/java/com/github/kapmahc/h2o/Application.java
+++ src/main/java/com/github/kapmahc/h2o/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class H2oApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(H2oApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
